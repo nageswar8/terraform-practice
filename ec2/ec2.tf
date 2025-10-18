9b0d21a0ad9fc5d317668b04fa744d083d101da0
nageswar8 <nageswarreddy447@gmail.com>
2025-10-18 19:02:02 +0530

    terraform init commit

---
+++ ec2/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform-demo" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_terraform.id]
+  
+  
+  tags = {
+    Name = "terraform-dev"
+    
+  }
+}
+
+resource "aws_security_group" "allow_all_terraform" {
+  name        = "allow_all_terraform"
+  description = "Allow all inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_all_terraform"
+  }
+}
+
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.allow_all_terraform.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.allow_all_terraform.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
